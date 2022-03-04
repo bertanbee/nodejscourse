e418a910882a65313d7b9ae8e44bc48d98ed4009
Lucas Bertan <bertan1707@gmail.com>
2022-03-04 17:44:55 -0300

    adding static loginpage

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,5 +6,6 @@ const viewController = require('../controllers/viewController');
 
 router.get('/', viewController.getOverview);
 router.get('/tour/:tour', viewController.getTour);
+router.get('/login', viewController.getLoginForm)
 
 module.exports = router;
